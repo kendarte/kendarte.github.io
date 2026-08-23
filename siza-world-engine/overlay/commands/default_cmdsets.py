fa67485d64632615c15f99e8a3ad9f8e09f9483f
kendarte <kendarte@gmail.com>
2026-08-23 12:29:17 -0600

    Register Siza runtime status command

--- siza-world-engine/overlay/commands/default_cmdsets.py
+++ siza-world-engine/overlay/commands/default_cmdsets.py
@@ -1,6 +1,6 @@
 from evennia import default_cmds
 
-from commands.siza_commands import CmdSizaNoMatch
+from commands.siza_commands import CmdSizaNoMatch, CmdSizaStatus
 
 
 class CharacterCmdSet(default_cmds.CharacterCmdSet):
@@ -8,6 +8,7 @@ class CharacterCmdSet(default_cmds.CharacterCmdSet):
 
     def at_cmdset_creation(self):
         super().at_cmdset_creation()
+        self.add(CmdSizaStatus())
         self.add(CmdSizaNoMatch())
 
 
