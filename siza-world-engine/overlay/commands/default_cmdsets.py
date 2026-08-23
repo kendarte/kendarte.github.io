55fd70bb5ac7dc09405e4ab6e573e48bb767fd13
kendarte <kendarte@gmail.com>
2026-08-23 01:54:03 -0600

    Wire Siza natural-language fallback into character cmdset

---
+++ siza-world-engine/overlay/commands/default_cmdsets.py
@@ -0,0 +1,32 @@
+from evennia import default_cmds
+
+from commands.siza_commands import CmdSizaNoMatch
+
+
+class CharacterCmdSet(default_cmds.CharacterCmdSet):
+    key = "DefaultCharacter"
+
+    def at_cmdset_creation(self):
+        super().at_cmdset_creation()
+        self.add(CmdSizaNoMatch())
+
+
+class AccountCmdSet(default_cmds.AccountCmdSet):
+    key = "DefaultAccount"
+
+    def at_cmdset_creation(self):
+        super().at_cmdset_creation()
+
+
+class UnloggedinCmdSet(default_cmds.UnloggedinCmdSet):
+    key = "DefaultUnloggedin"
+
+    def at_cmdset_creation(self):
+        super().at_cmdset_creation()
+
+
+class SessionCmdSet(default_cmds.SessionCmdSet):
+    key = "DefaultSession"
+
+    def at_cmdset_creation(self):
+        super().at_cmdset_creation()
