376b57e2d93218cf11cfff9941ead7ee3f322ae4
kendarte <kendarte@gmail.com>
2026-08-23 00:55:50 -0600

    Add Siza Character typeclass

---
+++ siza-world-engine/overlay/typeclasses/characters.py
@@ -0,0 +1,25 @@
+from evennia import DefaultCharacter
+
+
+class Character(DefaultCharacter):
+    """Player/NPC-compatible Siza character base."""
+
+    def at_object_creation(self):
+        super().at_object_creation()
+        self.db.siza_narration = True
+        self.db.adventure_stats = {
+            "FUE": 2,
+            "AGI": 2,
+            "COO": 2,
+            "INT": 2,
+            "PER": 2,
+            "PSI": 2,
+        }
+        self.db.knowledge = {}
+        self.db.virtues = {}
+        self.db.flaws = {}
+        self.db.needs = {}
+        self.db.relationships = {}
+        self.db.memories = []
+        self.db.current_action = None
+        self.db.destination_id = None
