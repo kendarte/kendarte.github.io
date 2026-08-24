09d6abb026cf74766aa4e61a3817015551a4e3e3
kendarte <kendarte@gmail.com>
2026-08-24 17:00:00 -0600

    Wire v0.80 conversation fact acquisition into default nomatch

--- siza-world-engine/overlay/commands/default_cmdsets.py
+++ siza-world-engine/overlay/commands/default_cmdsets.py
@@ -102,7 +102,7 @@ from commands.world_action_v42_commands import CmdSizaActionsV42, CmdSizaValidat
 from commands.world_action_v43_commands import CmdSizaValidateV43
 from commands.world_action_v44_commands import CmdSizaValidateV44
 from commands.world_action_v46_commands import CmdSizaValidateV46
-from commands.world_input_v79_commands import CmdSizaNoMatchV79
+from commands.world_input_v80_commands import CmdSizaNoMatchV80
 from commands.world_object_v47_commands import CmdSizaValidateV47
 from commands.world_object_v48_commands import CmdSizaValidateV48
 from commands.world_object_v49_commands import CmdSizaValidateV49
@@ -274,7 +274,7 @@ class CharacterCmdSet(default_cmds.CharacterCmdSet):
         self.add(CmdSizaTimeRate())
         self.add(CmdSizaTimeAdvance())
         self.add(CmdSizaRollV55())
-        self.add(CmdSizaNoMatchV79())
+        self.add(CmdSizaNoMatchV80())
 
 
 class AccountCmdSet(default_cmds.AccountCmdSet):
