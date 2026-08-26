0f9391c09f276d31a590454d46bba5526b30a316
kendarte <kendarte@gmail.com>
2026-08-25 18:16:32 -0600

    Add pure combat outcome planning

--- siza-core/creature-rules.js
+++ siza-core/creature-rules.js
@@ -53,6 +53,46 @@ function removeBattlefieldCreature(player,index,dest='graveyard'){
   return id;
 }
 
+function combatCounterGain(card,effectsForEvent){
+  const effectResolver=typeof effectsForEvent==='function'?effectsForEvent:()=>[];
+  return effectResolver(card,'combat-damage')
+    .filter(effect=>effect.type==='add-power-counter')
+    .reduce((sum,effect)=>sum+(effect.amount||0),0);
+}
+
+function combatPlan(combat,attackerPlayer,defenderPlayer,resolveCard,effectsForEvent){
+  const cardResolver=typeof resolveCard==='function'?resolveCard:()=>null;
+  const effectResolver=typeof effectsForEvent==='function'?effectsForEvent:()=>[];
+  const attackerDeaths=new Set(),defenderDeaths=new Set(),attackerGains=new Map(),defenderGains=new Map(),exchanges=[];
+  let damage=0;
+  for(let slot=0;slot<(combat?.attackers||[]).length;slot++){
+    const attacker=combat.attackers[slot],attackerCard=cardResolver(attackerPlayer?.battlefield?.[attacker.index]);
+    const attackerPower=effectivePowerValue(attackerPlayer,attacker.index,cardResolver,effectResolver),attackerToughness=toughnessValue(attackerPlayer,attacker.index,cardResolver);
+    const blockerIndex=combat?.blockers?.[String(slot)],blockerCard=blockerIndex==null?null:cardResolver(defenderPlayer?.battlefield?.[blockerIndex]);
+    if(blockerCard){
+      const blockerPower=effectivePowerValue(defenderPlayer,blockerIndex,cardResolver,effectResolver),blockerToughness=toughnessValue(defenderPlayer,blockerIndex,cardResolver);
+      if(attackerPower>=blockerToughness)defenderDeaths.add(blockerIndex);
+      if(blockerPower>=attackerToughness)attackerDeaths.add(attacker.index);
+      const attackerGain=attackerPower>0?combatCounterGain(attackerCard,effectResolver):0,defenderGain=blockerPower>0?combatCounterGain(blockerCard,effectResolver):0;
+      if(attackerGain)attackerGains.set(attacker.index,(attackerGains.get(attacker.index)||0)+attackerGain);
+      if(defenderGain)defenderGains.set(blockerIndex,(defenderGains.get(blockerIndex)||0)+defenderGain);
+      exchanges.push({attackerIndex:attacker.index,defenderIndex:blockerIndex,attackerName:attackerCard?.name||'',defenderName:blockerCard?.name||'',attackerPower,attackerToughness,defenderPower:blockerPower,defenderToughness:blockerToughness});
+    }else{
+      damage+=attackerPower;
+      const attackerGain=attackerPower>0?combatCounterGain(attackerCard,effectResolver):0;
+      if(attackerGain)attackerGains.set(attacker.index,(attackerGains.get(attacker.index)||0)+attackerGain);
+    }
+  }
+  return {
+    damage,
+    exchanges,
+    attackerDeaths:[...attackerDeaths],
+    defenderDeaths:[...defenderDeaths],
+    attackerCounterGains:[...attackerGains].filter(([index])=>!attackerDeaths.has(index)),
+    defenderCounterGains:[...defenderGains].filter(([index])=>!defenderDeaths.has(index))
+  };
+}
+
 global.SizaCreatureRules=Object.freeze({
   counter:counterValue,
   equipmentFor:equipmentForIndex,
@@ -60,6 +100,7 @@ global.SizaCreatureRules=Object.freeze({
   effectivePower:effectivePowerValue,
   toughness:toughnessValue,
   addCreature:addBattlefieldCreature,
-  removeAt:removeBattlefieldCreature
+  removeAt:removeBattlefieldCreature,
+  combatPlan
 });
 })(window);
