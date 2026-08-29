c3d0f9d8e3c2b3f65361ca23b902651b150c93de
kendarte <kendarte@gmail.com>
2026-08-28 21:23:41 -0600

    Refresh deck generator seed cache for shared core decks

--- siza-card-generator/rules-sync.js
+++ siza-card-generator/rules-sync.js
@@ -47,10 +47,10 @@ window.SizaRulesSync=Object.freeze({refresh:syncRules});
 
 (function(){
  const starter=document.createElement('script');
- starter.src='./starter-decks.js?v=dragon-thunder-v01';
+ starter.src='./starter-decks.js?v=core-decks-v02';
  starter.onload=()=>{
   const script=document.createElement('script');
-  script.src='./deck-generator.js?v=dragon-thunder-v01';
+  script.src='./deck-generator.js?v=core-decks-v02';
   document.body.appendChild(script);
  };
  document.body.appendChild(starter);
