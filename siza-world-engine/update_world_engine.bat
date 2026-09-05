5e3c75088bcdf56b3c58e3121b5d8b48218a7ed4
kendarte <kendarte@gmail.com>
2026-09-05 13:40:55 -0600

    siza: open room-only observation build after update

--- siza-world-engine/update_world_engine.bat
+++ siza-world-engine/update_world_engine.bat
@@ -4,7 +4,7 @@ cd /d "%~dp0"
 set "NO_PAUSE=0"
 if /I "%~1"=="/nopause" set "NO_PAUSE=1"
 set "SIZA_DARKHAVEN_MAP=%CD%\map-creator\presets\darkhaven-academy.siza-map.json"
-set "SIZA_WEBCLIENT_URL=http://127.0.0.1:4001/webclient/?siza_build=20260905-look-fallback&force=6"
+set "SIZA_WEBCLIENT_URL=http://127.0.0.1:4001/webclient/?siza_build=20260905-room-only-observation&force=7"
 
 echo ========================================
 echo SIZA WORLD ENGINE - UPDATE
