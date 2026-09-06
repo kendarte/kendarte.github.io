fed9095f37c4039f28f37415d3c809282a2a1839
kendarte <kendarte@gmail.com>
2026-09-06 00:07:54 -0600

    Register defensive reaction options command

--- pokerol/overlay/commands/default_cmdsets.py
+++ pokerol/overlay/commands/default_cmdsets.py
@@ -31,7 +31,7 @@ from commands.pokemon_battle_commands import (
     CmdPokerolPositionOptions,
 )
 from commands.pokemon_machine_commands import CmdPokerolEquipKnownMove, CmdPokerolMachines, CmdPokerolTeachMachine
-from commands.pokemon_reaction_commands import CmdPokerolBattleReaction
+from commands.pokemon_reaction_commands import CmdPokerolBattleReaction, CmdPokerolReactionOptions
 from commands.pokemon_registry_commands import (
     CmdPokerolGivePokemon,
     CmdPokerolPokemonRegistry,
@@ -83,7 +83,8 @@ class CharacterCmdSet(default_cmds.CharacterCmdSet):
             CmdPokerolParty(), CmdPokerolActivePokemon(), CmdPokerolBag(), CmdPokerolGiveItem(), CmdPokerolTrainerTest(),
             CmdPokerolMachines(), CmdPokerolTeachMachine(), CmdPokerolEquipKnownMove(),
             CmdPokerolPokemonRegistry(), CmdPokerolGivePokemon(), CmdPokerolWildEncounter(),
-            CmdPokerolBattleState(), CmdPokerolBattleTest(), CmdPokerolBattleAction(), CmdPokerolPositionOptions(), CmdPokerolBattleReaction(), CmdPokerolBattleMove(),
+            CmdPokerolBattleState(), CmdPokerolBattleTest(), CmdPokerolBattleAction(), CmdPokerolPositionOptions(),
+            CmdPokerolReactionOptions(), CmdPokerolBattleReaction(), CmdPokerolBattleMove(),
             CmdPokerolBattleCapture(), CmdPokerolBattleRun(), CmdPokerolBattleAbandon(),
             CmdPokerolDMStart(), CmdPokerolDMStatus(), CmdPokerolDMPlan(), CmdPokerolDMSignal(), CmdPokerolDMAdvance(),
             CmdSizaRoomState(), CmdSizaUiContext(), CmdSizaUiStats(),
